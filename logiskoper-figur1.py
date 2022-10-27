f96f94afb584fa929e84617497e8f2cfdf980419
Artjoms Donskojs <100723265+wayarma@users.noreply.github.com>
2022-10-27 15:56:19 +0300

    Create logiskoper-figur1.py

---
+++ logiskoper-figur1.py
@@ -0,0 +1,9 @@
+point_x = float(input("Ievadiet x kord.: "))
+point_y = float(input("Ievadiet y kord.: "))
+
+if point_x < -1 or point_x > 3 and point_y > 1 or point_y <- 2:
+    print("Punkts atrodas arpus robezas!")
+elif point_x == -1 or point_x == 3 or point_y == 1 or point_y == -2:
+    print("Punkts atrodas robezlinija!")
+else:
+    print("Punkts ir figuras ieksa!")
